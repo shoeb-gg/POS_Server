7a978271a64a48747306e53523c6b2c70dc03c9c
Shoeb Uddin Ahmed <shoeb.ua@gmail.com>
2025-05-02 23:47:42 +0600

    product model

--- src/models/product.DTO.ts
+++ src/models/product.DTO.ts
@@ -1,14 +1,13 @@
-model PRODUCTS {
-    id                String             
-    created_at        DateTime           
-    updated_at        DateTime            
-    name              String
-    quantity_type     String              
-    price_amount      Float?
-    price_unit        String?            
-    category          String?             
-    subcategory       String?             
-    description       String?
-    stockCount             String              
-  }
-  
+export type PRODUCTS = {
+  id: string;
+  created_at: Date;
+  updated_at: Date;
+  name: string;
+  quantity_type: string;
+  price_amount: number;
+  price_unit: string;
+  category: string;
+  subcategory: string;
+  description: string;
+  stockCount: string;
+};
