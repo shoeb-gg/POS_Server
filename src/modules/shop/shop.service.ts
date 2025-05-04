65436e7e2986bc4daf5da86d6fcb004cca38c147
Shoeb Uddin Ahmed <shoeb.ua@gmail.com>
2025-05-05 03:30:24 +0600

    shop crud complete

--- src/modules/shop/shop.service.ts
+++ src/modules/shop/shop.service.ts
@@ -33,19 +33,126 @@ export class ShopService {
     }
   }
 
-  findAll() {
-    return `This action returns all shop`;
+  async findAll(): Promise<ResponseDto> {
+    try {
+      const shops: Shop[] = await this.prisma.shop.findMany();
+
+      return {
+        data: shops,
+        success: true,
+        message: 'Shops Fetched Successfully!',
+        status: shops.length === 0 ? HttpStatus.NO_CONTENT : HttpStatus.OK,
+      };
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        'Server Error while fetching all Shops!',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} shop`;
+  async findAllForUser(userId: number): Promise<ResponseDto> {
+    try {
+      const shops: Shop[] = await this.prisma.shop.findMany({
+        where: {
+          user_id: userId,
+        },
+      });
+
+      return {
+        data: shops,
+        success: shops.length !== 0 ? true : false,
+        message:
+          shops.length !== 0
+            ? 'Shops Fetched Successfully!'
+            : `No Shop found for user `,
+        status: shops.length === 0 ? HttpStatus.NO_CONTENT : HttpStatus.OK,
+      };
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        'Server Error while fetching Shops!',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  update(id: number, updateShopDto: UpdateShopDto) {
-    return `This action updates a #${id} shop`;
+  async findOne(id: number): Promise<ResponseDto> {
+    try {
+      const shop: Shop | null = await this.prisma.shop.findUnique({
+        where: {
+          id: id,
+        },
+      });
+
+      return {
+        data: shop ? shop : {},
+        success: shop ? true : false,
+        message: shop
+          ? 'Shop Fetched Successfully!'
+          : `No Shop Found with id ${id}`,
+        status: shop ? HttpStatus.OK : HttpStatus.NO_CONTENT,
+      };
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        'Server Error while fetching Shop!',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} shop`;
+  async update(id: number, updateShopDto: UpdateShopDto): Promise<ResponseDto> {
+    try {
+      const shop: Shop | null = await this.prisma.shop.update({
+        data: {
+          ...updateShopDto,
+        },
+        where: {
+          id: id,
+        },
+      });
+
+      return {
+        data: shop ? shop : {},
+        success: shop ? true : false,
+        message: shop
+          ? 'Shop updated Successfully!'
+          : `No Shop Found with id ${id}`,
+        status: shop ? HttpStatus.OK : HttpStatus.NO_CONTENT,
+      };
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        'Server Error while updating Shop!',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async remove(id: number): Promise<ResponseDto> {
+    try {
+      const shop: Shop | null = await this.prisma.shop.delete({
+        where: {
+          id: id,
+        },
+      });
+
+      return {
+        data: {},
+        success: shop ? true : false,
+        message: shop
+          ? 'Shop deleted Successfully!'
+          : `No Shop Found with id ${id}`,
+        status: shop ? HttpStatus.OK : HttpStatus.BAD_REQUEST,
+      };
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        'Server Error while deleting Shop!',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
