6ec7b4304d813f4387ceaca4968da78fa1d0c374
Shoeb Uddin Ahmed <shoeb.ua@gmail.com>
2025-05-29 15:01:15 +0600

    cusrtomer crud done

--- src/modules/entities/customer/customer.service.ts
+++ src/modules/entities/customer/customer.service.ts
@@ -2,7 +2,10 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreateCustomerDto } from './dto/create-customer.dto';
 import { UpdateCustomerDto } from './dto/update-customer.dto';
 import { PrismaService } from 'src/prisma.service';
-import { ResponseDto } from 'src/common/models/response.dto';
+import {
+  FindAllResponseDto,
+  ResponseDto,
+} from 'src/common/models/response.dto';
 import { Customer } from './entities/customer.entity';
 
 @Injectable()
@@ -44,19 +47,168 @@ export class CustomerService {
     }
   }
 
-  findAll() {
-    return `This action returns all customer`;
+  async findAll(
+    userId: number,
+    pageNumber: number,
+    pageSize: number,
+    query: string = '',
+  ): Promise<FindAllResponseDto<Customer[]>> {
+    try {
+      const conditions: any = {
+        user_id: userId,
+        name: {
+          contains: query,
+          mode: 'insensitive',
+        },
+      };
+
+      const [customers, total]: [Customer[], number] =
+        await this.prisma.$transaction([
+          this.prisma.customer.findMany({
+            where: conditions,
+            skip: (pageNumber - 1) * pageSize,
+            take: pageSize,
+            orderBy: {
+              name: 'asc',
+            },
+            select: {
+              id: true,
+              name: true,
+              email: true,
+              phone: true,
+              country: true,
+              city: true,
+              address: true,
+              created_on: true,
+              updated_on: true,
+            },
+          }),
+          this.prisma.customer.count({
+            where: conditions,
+          }),
+        ]);
+
+      return {
+        data: customers,
+        total: total,
+        pagination: {
+          currentPage: pageNumber,
+          pageSize: pageSize,
+          totalPages: Math.ceil(total / pageSize),
+        },
+        success: true,
+        message: 'Customers Fetched Successfully!',
+        status: customers.length === 0 ? HttpStatus.NO_CONTENT : HttpStatus.OK,
+      };
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        'Server Error while fetching all Customers!',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} customer`;
+  async findOne(id: number): Promise<ResponseDto<Customer | null>> {
+    try {
+      const customer: Customer | null = await this.prisma.customer.findUnique({
+        where: {
+          id: id,
+        },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          phone: true,
+          country: true,
+          city: true,
+          address: true,
+          created_on: true,
+          updated_on: true,
+        },
+      });
+
+      return {
+        data: customer ? customer : null,
+        success: customer ? true : false,
+        message: customer
+          ? 'Customer Fetched Successfully!'
+          : `No Customer Found with id ${id}`,
+        status: customer ? HttpStatus.OK : HttpStatus.NO_CONTENT,
+      };
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        'Server Error while fetching Customer!',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  update(id: number, updateCustomerDto: UpdateCustomerDto) {
-    return `This action updates a #${id} customer`;
+  async update(
+    id: number,
+    updateCustomerDto: UpdateCustomerDto,
+  ): Promise<ResponseDto<Customer | null>> {
+    try {
+      const customer: Customer | null = await this.prisma.customer.update({
+        data: {
+          ...updateCustomerDto,
+        },
+        where: {
+          id: id,
+        },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          phone: true,
+          country: true,
+          city: true,
+          address: true,
+          created_on: true,
+          updated_on: true,
+        },
+      });
+
+      return {
+        data: customer ? customer : null,
+        success: customer ? true : false,
+        message: customer
+          ? 'Customer updated Successfully!'
+          : `No Customer Found with id ${id}`,
+        status: customer ? HttpStatus.OK : HttpStatus.NO_CONTENT,
+      };
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        'Server Error while updating Customer!',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} customer`;
+  async remove(id: number): Promise<ResponseDto<null>> {
+    try {
+      const customer: Customer | null = await this.prisma.customer.delete({
+        where: {
+          id: id,
+        },
+      });
+
+      return {
+        data: null,
+        success: customer ? true : false,
+        message: customer
+          ? 'Customer deleted Successfully!'
+          : `No Customer Found with id ${id}`,
+        status: customer ? HttpStatus.OK : HttpStatus.BAD_REQUEST,
+      };
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        'Server Error while deleting Customer!',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
   }
 }
